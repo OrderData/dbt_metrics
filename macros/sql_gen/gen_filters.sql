906b1dc4d8db60ab5959e0c89e33f991ca00ee85
jaimefr <jaimefarres@gmail.com>
2023-06-16 20:02:28 +0200

    Improve

--- macros/sql_gen/gen_filters.sql
+++ macros/sql_gen/gen_filters.sql
@@ -8,12 +8,12 @@
     {% if start_date or end_date %}
         and (
         {% if start_date and end_date -%}
-            cast(base_model.{{model_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date) >= cast('{{ start_date }}' as date)
-            and cast(base_model.{{model_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date) < cast('{{ end_date }}' as date)
+            cast(base_model.{{model_values.timestamp | replace( '<<date_grain>>' , 'week' ) }} as date) >= cast('{{ start_date }}' as date)
+            and cast(base_model.{{model_values.timestamp | replace( '<<date_grain>>' , 'week' ) }} as date) < cast('{{ end_date }}' as date)
         {%- elif start_date and not end_date -%}
-            cast(base_model.{{model_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date) >= cast('{{ start_date }}' as date)
+            cast(base_model.{{model_values.timestamp | replace( '<<date_grain>>' , 'week' ) }} as date) >= cast('{{ start_date }}' as date)
         {%- elif end_date and not start_date -%}
-            cast(base_model.{{model_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date) < cast('{{ end_date }}' as date)
+            cast(base_model.{{model_values.timestamp | replace( '<<date_grain>>' , 'week' ) }} as date) < cast('{{ end_date }}' as date)
         {%- endif %} 
         )
     {% endif -%} 
