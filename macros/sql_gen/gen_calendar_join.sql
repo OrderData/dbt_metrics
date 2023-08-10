16e75c0677217d7b74bbdd3bc2c25fc5951bf1dc
jaimefr <jaimefarres@gmail.com>
2023-08-10 21:21:24 +0200

    Improve joins

--- macros/sql_gen/gen_calendar_join.sql
+++ macros/sql_gen/gen_calendar_join.sql
@@ -5,39 +5,39 @@
 {% macro default__gen_calendar_join(group_values, grain) %}
         left join calendar
         {%- if group_values.window is not none %}
-            on cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as {%- if grain in ['hour'] %} timestamp{%- else -%} date{%- endif -%}) > dateadd({{group_values.window.period}}, -{{group_values.window.count}}, calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%})
-            and cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as {%- if grain in ['hour'] %} timestamp{%- else -%} date{%- endif -%}) < calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%}
+            on {%- if grain in ['hour'] %}{{ date_trunc('minute', 'base_model' || group_values.timestamp | replace( '<<date_grain>>' , grain )) }}{%- else -%}cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date){%- endif -%} > dateadd({{group_values.window.period}}, -{{group_values.window.count}}, calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%})
+            and {%- if grain in ['hour'] %}{{ date_trunc('minute', 'base_model' || group_values.timestamp | replace( '<<date_grain>>' , grain )) }}{%- else -%}cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date){%- endif -%} < calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%}
         {%- else %}
-            on cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as {%- if grain in ['hour'] %} timestamp{%- else -%} date{%- endif -%}) = calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%}
+            on {%- if grain in ['hour'] %}{{ date_trunc('minute', 'base_model' || group_values.timestamp | replace( '<<date_grain>>' , grain )) }}{%- else -%}cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date){%- endif -%} = calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%}
         {% endif -%}
 {% endmacro %}
 
 {% macro bigquery__gen_calendar_join(group_values, grain) %}
         left join calendar
         {%- if group_values.window is not none %}
-            on cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as {%- if grain in ['hour'] %} timestamp{%- else -%} date{%- endif -%}) > date_sub(calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%}, interval {{group_values.window.count}} {{group_values.window.period}})
-            and cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as {%- if grain in ['hour'] %} timestamp{%- else -%} date{%- endif -%}) < calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%}
+            on {%- if grain in ['hour'] %}{{ date_trunc('minute', 'base_model' || group_values.timestamp | replace( '<<date_grain>>' , grain )) }}{%- else -%}cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date){%- endif -%} > date_sub(calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%}, interval {{group_values.window.count}} {{group_values.window.period}})
+            and {%- if grain in ['hour'] %}{{ date_trunc('minute', 'base_model' || group_values.timestamp | replace( '<<date_grain>>' , grain )) }}{%- else -%}cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date){%- endif -%} < calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%}
         {%- else %}
-            on cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as {%- if grain in ['hour'] %} timestamp{%- else -%} date{%- endif -%}) = calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%}
+            on {%- if grain in ['hour'] %}{{ date_trunc('minute', 'base_model' || group_values.timestamp | replace( '<<date_grain>>' , grain )) }}{%- else -%}cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date){%- endif -%} = calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%}
         {% endif -%}
 {% endmacro %}
 
 {% macro postgres__gen_calendar_join(group_values, grain) %}
         left join calendar
         {%- if group_values.window is not none %}
-            on cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as {%- if grain in ['hour'] %} timestamp{%- else -%} date{%- endif -%}) > calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%} - interval '{{group_values.window.count}} {{group_values.window.period}}'
-            and cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as {%- if grain in ['hour'] %} timestamp{%- else -%} date{%- endif -%}) < calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%}
+            on {%- if grain in ['hour'] %}{{ date_trunc('minute', 'base_model' || group_values.timestamp | replace( '<<date_grain>>' , grain )) }}{%- else -%}cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date){%- endif -%} > calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%} - interval '{{group_values.window.count}} {{group_values.window.period}}'
+            and {%- if grain in ['hour'] %}{{ date_trunc('minute', 'base_model' || group_values.timestamp | replace( '<<date_grain>>' , grain )) }}{%- else -%}cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date){%- endif -%} < calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%}
         {%- else %}
-            on cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as {%- if grain in ['hour'] %} timestamp{%- else -%} date{%- endif -%}) = calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%}
+            on {%- if grain in ['hour'] %}{{ date_trunc('minute', 'base_model' || group_values.timestamp | replace( '<<date_grain>>' , grain )) }}{%- else -%}cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date){%- endif -%} = calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%}
         {% endif -%}
 {% endmacro %}
 
 {% macro redshift__gen_calendar_join(group_values, grain) %}
         left join calendar
         {%- if group_values.window is not none %}
-            on cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as {%- if grain in ['hour'] %} timestamp{%- else -%} date{%- endif -%}) > dateadd({{group_values.window.period}}, -{{group_values.window.count}}, calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%})
-            and cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as {%- if grain in ['hour'] %} timestamp{%- else -%} date{%- endif -%}) < calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%}
+            on {%- if grain in ['hour'] %}{{ date_trunc('minute', 'base_model' || group_values.timestamp | replace( '<<date_grain>>' , grain )) }}{%- else -%}cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date){%- endif -%} > dateadd({{group_values.window.period}}, -{{group_values.window.count}}, calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%})
+            and {%- if grain in ['hour'] %}{{ date_trunc('minute', 'base_model' || group_values.timestamp | replace( '<<date_grain>>' , grain )) }}{%- else -%}cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date){%- endif -%} < calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%}
         {%- else %}
-            on cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as {%- if grain in ['hour'] %} timestamp{%- else -%} date{%- endif -%}) = calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%}
+            on {%- if grain in ['hour'] %}{{ date_trunc('minute', 'base_model' || group_values.timestamp | replace( '<<date_grain>>' , grain )) }}{%- else -%}cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date){%- endif -%} = calendar.date_{%- if grain in ['hour'] %}minute{%- else -%}day{%- endif -%}
         {% endif -%}
 {% endmacro %}
