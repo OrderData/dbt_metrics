1e8398470119e1f5086f2839a73a7b043a4fd4df
jaimefr <jaimefarres@gmail.com>
2023-08-10 22:26:53 +0200

    Improve date filters

--- macros/sql_gen/gen_filters.sql
+++ macros/sql_gen/gen_filters.sql
@@ -4,16 +4,25 @@
 
 {%- macro default__gen_filters(model_values, start_date, end_date, where, grain) -%}
 
+    {%- if grain in ['hour','minute'] %}
+        {%- set calendar_date_type = 'timestamp' -%}
+    {%- else -%}
+        {%- set calendar_date_type = 'date' -%}
+    {%- endif %}
+
     {#- metric start/end dates also applied here to limit incoming data -#}
     {% if start_date or end_date %}
         and (
         {% if start_date and end_date -%}
-            cast(base_model.{{model_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date) >= cast('{{ start_date }}' as date)
-            and cast(base_model.{{model_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date) < cast('{{ end_date }}' as date)
+            {%- if grain in ['hour','minute'] %}
+            cast(base_model.{{model_values.timestamp | replace( '<<date_grain>>' , grain ) }} as {{calendar_date_type}}) >= cast('{{ start_date }}' as {{calendar_date_type}})
+            and cast(base_model.{{model_values.timestamp | replace( '<<date_grain>>' , grain ) }} as {{calendar_date_type}}) < cast('{{ end_date }}' as {{calendar_date_type}})
+            {%- else -%}
+            {%- endif %}
         {%- elif start_date and not end_date -%}
-            cast(base_model.{{model_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date) >= cast('{{ start_date }}' as date)
+            cast(base_model.{{model_values.timestamp | replace( '<<date_grain>>' , grain ) }} as {{calendar_date_type}}) >= cast('{{ start_date }}' as {{calendar_date_type}})
         {%- elif end_date and not start_date -%}
-            cast(base_model.{{model_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date) < cast('{{ end_date }}' as date)
+            cast(base_model.{{model_values.timestamp | replace( '<<date_grain>>' , grain ) }} as {{calendar_date_type}}) < cast('{{ end_date }}' as {{calendar_date_type}})
         {%- endif %} 
         )
     {% endif -%} 
