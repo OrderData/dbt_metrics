1200294983126f9322fc1e606891053ca5a64e0f
jaimefr <jaimefarres@gmail.com>
2023-08-10 19:11:04 +0200

    Fix

--- macros/sql_gen/gen_base_query.sql
+++ macros/sql_gen/gen_base_query.sql
@@ -14,7 +14,7 @@
             -#}
             cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as {%- if grain in ['hour'] %} timestamp {%- else -%} date {%- endif -%}) as metric_date_{%- if grain in ['hour'] %}minute {%- else -%}day {%- endif -%},
             calendar.date_{{ grain }} as date_{{grain}},
-            calendar.date_{%- if grain in ['hour'] %} minute {%- else -%} day {%- endif -%} as window_filter_date,
+            calendar.date_{%- if grain in ['hour'] %}minute {%- else -%}day {%- endif -%}  as window_filter_date,
                 {%- if secondary_calculations | length > 0 %}
                     {%- for period in relevant_periods %}
             calendar.date_{{ period }},
