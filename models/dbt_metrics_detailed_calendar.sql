2c97cf04e42ccfc0fb82c10bd173079039549289
jaimefr <jaimefarres@gmail.com>
2023-08-10 18:46:25 +0200

    Create dbt_metrics_detailed_calendar.sql

---
+++ models/dbt_metrics_detailed_calendar.sql
@@ -0,0 +1,19 @@
+{{ config(materialized='table') }}
+
+with days as (
+    {{ metrics.metric_date_spine(
+    datepart="minute",
+    start_date="cast('1990-01-01' as date)",
+    end_date="cast('2030-01-01' as date)"
+   )
+    }}
+),
+
+final as (
+    select 
+        date_minute,
+        cast({{ date_trunc('minute', 'date_minute') }} as date) as date_hour
+    from days
+)
+
+select * from final
