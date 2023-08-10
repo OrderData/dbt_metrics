5017fb430c52899d9c35a335a58a06dfbb990acb
jaimefr <jaimefarres@gmail.com>
2023-08-10 21:38:10 +0200

    Improve

--- macros/sql_gen/gen_calendar_join.sql
+++ macros/sql_gen/gen_calendar_join.sql
@@ -13,7 +13,7 @@
 {% endmacro %}
 
 {% macro bigquery__gen_calendar_join(group_values, grain) %}
-        left join calendar 
+        left join calendar{{' '}}
         {%- if group_values.window is not none %}
             {%- if grain in ['hour','minute'] %} on {{ date_trunc('minute', 'base_model.' + group_values.timestamp | replace( '<<date_grain>>' , grain )) + ' ' }} {%- else -%} on cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date) {%- endif -%} > date_sub(calendar.date_{%- if grain in ['hour','minute'] %}minute{%- else -%}day{%- endif -%}, interval {{group_values.window.count}} {{group_values.window.period}})
             {%- if grain in ['hour','minute'] %} and {{ date_trunc('minute', 'base_model.' + group_values.timestamp | replace( '<<date_grain>>' , grain )) + ' ' }} {%- else -%} and cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date) {%- endif -%} < calendar.date_{%- if grain in ['hour','minute'] %}minute{%- else -%}day{%- endif -%}
@@ -23,7 +23,7 @@
 {% endmacro %}
 
 {% macro postgres__gen_calendar_join(group_values, grain) %}
-        left join calendar 
+        left join calendar{{' '}}
         {%- if group_values.window is not none %}
             {%- if grain in ['hour','minute'] %} on {{ date_trunc('minute', 'base_model.' + group_values.timestamp | replace( '<<date_grain>>' , grain )) + ' ' }} {%- else -%} on cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date) {%- endif -%} > calendar.date_{%- if grain in ['hour','minute'] %}minute{%- else -%}day{%- endif -%} - interval '{{group_values.window.count}} {{group_values.window.period}}'
             {%- if grain in ['hour','minute'] %} and {{ date_trunc('minute', 'base_model.' + group_values.timestamp | replace( '<<date_grain>>' , grain )) + ' ' }} {%- else -%} and cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date) {%- endif -%} < calendar.date_{%- if grain in ['hour','minute'] %}minute{%- else -%}day{%- endif -%}
@@ -33,7 +33,7 @@
 {% endmacro %}
 
 {% macro redshift__gen_calendar_join(group_values, grain) %}
-        left join calendar 
+        left join calendar{{' '}}
         {%- if group_values.window is not none %}
             {%- if grain in ['hour','minute'] %} on {{ date_trunc('minute', 'base_model.' + group_values.timestamp | replace( '<<date_grain>>' , grain )) + ' ' }} {%- else -%} on cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date) {%- endif -%} > dateadd({{group_values.window.period}}, -{{group_values.window.count}}, calendar.date_{%- if grain in ['hour','minute'] %}minute{%- else -%}day{%- endif -%})
             {%- if grain in ['hour','minute'] %} and {{ date_trunc('minute', 'base_model.' + group_values.timestamp | replace( '<<date_grain>>' , grain )) + ' ' }} {%- else -%} and cast(base_model.{{group_values.timestamp | replace( '<<date_grain>>' , grain ) }} as date) {%- endif -%} < calendar.date_{%- if grain in ['hour','minute'] %}minute{%- else -%}day{%- endif -%}
